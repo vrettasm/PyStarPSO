199add4888e4ce04a6beddbbfab33974f61f8f91
vrettasm <vrettasm@gmail.com>
2025-05-22 16:54:40 +0200

    Initial commit.

---
+++ ppso/auxiliary/swarm.py
@@ -0,0 +1,15 @@
+import numpy as np
+from copy import deepcopy
+
+class Swarm(object):
+    """
+    Description:
+        Models the Swarm as a population (list) of Particles.
+    """
+
+    # Object variables.
+    __slots__ = ("_population", "_func", "_valid")
+
+    pass
+
+# _end_class_
