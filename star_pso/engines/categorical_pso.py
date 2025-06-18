4e9d2ae7b0dadbdbbdb05f577d4055b3ddcdd977
vrettasm <vrettasm@gmail.com>
2025-06-18 12:34:51 +0200

    Update documentation.

--- star_pso/engines/categorical_pso.py
+++ star_pso/engines/categorical_pso.py
@@ -211,7 +211,7 @@ class CategoricalPSO(GenericPSO):
     def run(self, max_it: int = 100, f_tol: float = None, options: dict = None,
             parallel: bool = False, reset_swarm: bool = False, verbose: bool = False) -> None:
         """
-        Main method of the StandardPSO class, that implements the optimization routine.
+        Main method of the CategoricalPSO class, that implements the optimization routine.
 
         :param max_it: (int) maximum number of iterations in the optimization loop.
 
