e79fcf535d45df04a4ac505b85086fddc148b0c8
vrettasm <vrettasm@gmail.com>
2025-06-19 16:06:03 +0200

    Added tests for the data_block class.

--- tests/test_data_block.py
+++ tests/test_data_block.py
@@ -1,6 +1,7 @@
 import unittest
 
 import numpy as np
+from star_pso.auxiliary.utilities import BlockType
 from star_pso.auxiliary.data_block import DataBlock
 
 
@@ -32,6 +33,166 @@ class TestDataBlock(unittest.TestCase):
                           upper_bound=+1.0)
         # _end_with_
     # _end_def_
+
+    def test_position_float(self):
+        """
+        Test whether the upd_float() returns a float variable.
+
+        :return: None.
+        """
+
+        # Create a test data block (FLOAT).
+        blk = DataBlock(position=self.rng.random(),
+                        btype=BlockType.FLOAT,
+                        lower_bound=0.0,
+                        upper_bound=1.0)
+
+        # Here we test the upd_float().
+        blk.position = self.rng.random()
+        self.assertTrue(isinstance(blk.position, float))
+    # _end_def_
+
+    def test_position_integer(self):
+        """
+        Test whether the upd_integer() returns an integer variable.
+
+        :return: None.
+        """
+
+        # Create a test data block (INTEGER).
+        blk = DataBlock(position=self.rng.integers(-5, 5),
+                        btype=BlockType.INTEGER,
+                        lower_bound=-10,
+                        upper_bound=+10)
+
+        # Here we test the upd_integer().
+        blk.position = self.rng.random()
+        self.assertTrue(isinstance(blk.position, int))
+    # _end_def_
+
+    def test_position_binary(self):
+        """
+        Test whether the upd_binary() returns a binary variable.
+
+        :return: None.
+        """
+
+        # Create a test data block (BINARY).
+        blk = DataBlock(position=self.rng.integers(0, 2),
+                        btype=BlockType.BINARY)
+
+        # Here we test the upd_binary().
+        blk.position = self.rng.random()
+        self.assertTrue(blk.position in (0, 1))
+    # _end_def_
+
+    def test_position_categorical(self):
+        """
+        Test whether the upd_categorical() returns
+        a vector of probabilities that sum to one.
+
+        :return: None.
+        """
+        # Test variable set.
+        var_set = ["a", "b", "c"]
+
+        # Get the size of the vector.
+        D = len(var_set)
+
+        # Create a test data block (CATEGORICAL).
+        blk = DataBlock(np.ones(D)/D,
+                        BlockType.CATEGORICAL,
+                        valid_set=var_set)
+
+        # Here we test the upd_categorical().
+        blk.position = self.rng.random(D)
+        self.assertAlmostEqual(np.sum(blk.position), 1.0)
+    # _end_def_
+
+    def test_reset_position_float(self):
+        """
+        Test whether the init_float() returns a float variable.
+
+        :return: None.
+        """
+
+        # Create a test data block (FLOAT).
+        blk = DataBlock(position=self.rng.random(),
+                        btype=BlockType.FLOAT,
+                        lower_bound=0.0,
+                        upper_bound=1.0)
+
+        # Here we test the init_float().
+        blk.reset_position()
+
+        self.assertTrue(0.0 <= blk.position <= 1.0)
+        self.assertTrue(isinstance(blk.position, float))
+    # _end_def_
+
+    def test_reset_position_integer(self):
+        """
+        Test whether the init_integer() returns
+        an integer variable within its limits.
+
+        :return: None.
+        """
+        # Create a test data block (INTEGER).
+        blk = DataBlock(position=self.rng.integers(-5, 5),
+                        btype=BlockType.INTEGER,
+                        lower_bound=-10,
+                        upper_bound=+10)
+
+        # Here we test the init_integer().
+        blk.reset_position()
+
+        self.assertTrue(-10 <= blk.position <= 10)
+        self.assertTrue(isinstance(blk.position, int))
+    # _end_def_
+
+    def test_reset_position_binary(self):
+        """
+        Test whether the init_binary() returns
+        a binary variable, within (0, 1).
+
+        :return: None.
+        """
+
+        # Create a test data block (BINARY).
+        blk = DataBlock(position=self.rng.integers(0, 2, dtype=int),
+                        btype=BlockType.BINARY)
+
+        # Here we test the init_binary().
+        blk.reset_position()
+
+        self.assertTrue(blk.position in (0, 1))
+        self.assertTrue(isinstance(blk.position, int))
+    # _end_def_
+
+    def test_rest_position_categorical(self):
+        """
+        Test whether the init_categorical() returns
+        a vector of probabilities that sum to one.
+
+        :return: None.
+        """
+        # Test variable set.
+        var_set = ["a", "b", "c"]
+
+        # Get the size of the vector.
+        D = len(var_set)
+
+        # Create a test data block (CATEGORICAL).
+        blk = DataBlock(np.ones(D)/D,
+                        BlockType.CATEGORICAL,
+                        valid_set=var_set)
+
+        # Here we test the init_categorical().
+        blk.reset_position()
+
+        self.assertAlmostEqual(np.sum(blk.position), 1.0)
+    # _end_def_
+
+# _end_class_
 
 
 if __name__ == '__main__':
