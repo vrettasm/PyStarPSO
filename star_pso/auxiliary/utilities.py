e9e25744050f6b800aa8577eb5d5bcaec8af4078
michailvrettas <vrettasm@gmail.com>
2025-11-07 19:06:22 +0100

    Update VOptions with new field (mode).

--- star_pso/auxiliary/utilities.py
+++ star_pso/auxiliary/utilities.py
@@ -16,12 +16,12 @@ ScalarOrArray = Union[int, float, ArrayLike]
 
 # Declare a named tuple with the parameters
 # we want to use in the velocity equations:
-# 1) 'w0': inertia weight
-# 2) 'c1': cognitive coefficient
-# 3) 'c2': social coefficient
-# 4) 'fipso': fully informed PSO (optional).
+# 1) "w0": inertia weight
+# 2) "c1": cognitive coefficient
+# 3) "c2": social coefficient
+# 4) "mode": "fipso", "g_best", "multimodal".
 VOptions = namedtuple("VOptions",
-                      ["w0", "c1", "c2", "fipso"], defaults=[False])
+                      ["w0", "c1", "c2", "mode"], defaults=[False])
 
 class BlockType(Enum):
     """
