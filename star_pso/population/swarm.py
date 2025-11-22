0f2d42febe04b12d02ee87ea4b6f9c4a30ba5189
michailvrettas <vrettasm@gmail.com>
2025-11-22 17:04:36 +0100

    Update import statements.

--- star_pso/population/swarm.py
+++ star_pso/population/swarm.py
@@ -8,9 +8,9 @@ from numpy import ndarray
 from numpy import asarray
 from numpy.typing import ArrayLike
 
-from star_pso.auxiliary.particle import Particle
+from star_pso.population.particle import Particle
 from star_pso.auxiliary.utilities import BlockType
-from star_pso.auxiliary.jat_particle import JatParticle
+from star_pso.population.jat_particle import JatParticle
 
 # Make a type alias for the particles types.
 SwarmParticle = Union[Particle, JatParticle]
