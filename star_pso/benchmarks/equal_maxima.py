e868b011d5bfa48d77febef2087cdbe60784f2d1
michailvrettas <vrettasm@gmail.com>
2025-11-23 00:51:21 +0100

    Initial commit.

---
+++ star_pso/benchmarks/equal_maxima.py
@@ -0,0 +1,62 @@
+import numpy as np
+from star_pso.benchmarks.test_function import TestFunction
+
+
+class EqualMaxima(TestFunction):
+    """
+    This function was originally proposed in:
+
+    K. Deb, “Genetic algorithms in multimodal function optimization
+    (master thesis and tcga report no. 89002),” Ph.D. dissertation,
+    Tuscaloosa: University of Alabama, The Clearinghouse for Genetic
+    Algorithms, 1989.
+    """
+
+    def __init__(self) -> None:
+        """
+        Default initializer of the FiveUnevenPeakTrap class.
+        """
+
+        # Call the super initializer with the name.
+        super().__init__(name="Equal_Maxima",
+                         x_min=0.0, x_max=1.0)
+    # _end_def_
+
+    def func(self, x_pos: np.ndarray) -> np.ndarray:
+        """
+        This is 1D function. There are 5 global optima.
+
+        :param x_pos: the current position(s) of the function.
+
+        :return: the function value(s).
+        """
+
+        # Initialize function values to NaN
+        f_value = np.full_like(x_pos, np.nan, dtype=float)
+
+        # Conditions for the different ranges.
+        in_range = (0.0 <= x_pos) & (x_pos <= 1.0)
+
+        # Vectorized calculations based on conditions.
+        f_value[in_range] = np.sin(5.0 * np.pi *x_pos[in_range])**6
+
+        # Return the ndarray.
+        return f_value
+    # _end_def_
+
+    def initial_random_positions(self, n_pos: int = 50) -> np.ndarray:
+        """
+        Generate the initial set of random positions within the minimum
+        / maximum bounds of the test problem.
+
+        :param n_pos: the number of positions to generate.
+
+        :return: a uniformly sampled set of random positions.
+        """
+        # Draw uniform random samples for the initial points.
+        return self.rng.uniform(self._x_min,
+                                self._x_max,
+                                size=(n_pos, 1))
+    # _end_def_
+
+# _end_class_
