e81b40f1ae086e35d8e1a0cf41e344db40d5de43
michailvrettas <vrettasm@gmail.com>
2025-11-23 13:27:06 +0100

    Generalize Shubert to nDim.

--- star_pso/benchmarks/shubert_2D.py
+++ star_pso/benchmarks/shubert_2D.py
@@ -10,19 +10,27 @@ class Shubert2D(TestFunction):
     New York: Springer-Verlag, New York, 1996.
     """
 
-    def __init__(self) -> None:
+    def __init__(self, n_dim: int = 2) -> None:
         """
         Default initializer of the SixHumpCamelBack class.
+
+        :param n_dim: Number of dimensions of the problem.
+
+        :return: None.
         """
 
         # Call the super initializer with the name.
         super().__init__(name="Shubert_2D",
                          x_min=-10.0, x_max=+10.0)
+
+        # Assign the number of dimensions.
+        self.n_dim = max(int(n_dim), 2)
     # _end_def_
 
     def func(self, x_pos: np.ndarray) -> np.ndarray:
         """
-        This is a 2D function with 18 global optimal values.
+        This is a multidimensional function with 'n_dim * 3^n_dim'
+        global optimal values.
 
         :param x_pos: the current position(s) of the function.
 
@@ -32,26 +40,20 @@ class Shubert2D(TestFunction):
         # Initialize function values to NaN.
         f_value = float("NaN")
 
-        # Separate the two variables.
-        x, y = x_pos
-
-        # Conditions for the different ranges.
-        x_range = (-10.0 <= x) & (x <= +10.0)
-        y_range = (-10.0 <= y) & (y <= +10.0)
-
-        # Vectorized calculations based on the condition.
-        if x_range & y_range:
+        # Check the valid function range.
+        if np.all((-10.0 <= x_pos) & (x_pos <= +10.0)):
             # Range 1 to 6.
             i = np.arange(1, 6)
 
-            # Calculate the first summation over each x.
-            sum_x = np.sum(i * np.cos((i + 1) * x + i), axis=0)
+            # Set the initial value to one.
+            sum_x = np.ones(1)
 
-            # Calculate the second summation over each y.
-            sum_y = np.sum(i * np.cos((i + 1) * y + i), axis=0)
+            # Calculate the summation over each x.
+            for xi in x_pos:
+                sum_x *= np.sum(i * np.cos((i + 1) * xi + i), axis=0)
 
             # Get the product of both sums.
-            f_value = - (sum_x * sum_y)
+            f_value = -sum_x
         # _end_if_
 
         # Return the ndarray.
