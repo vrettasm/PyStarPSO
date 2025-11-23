dab49b099c6cf1e8aa49fac277f297a7a69987b4
michailvrettas <vrettasm@gmail.com>
2025-11-23 12:12:34 +0100

    Initial commit.

---
+++ star_pso/benchmarks/six_hump_camel_back.py
@@ -0,0 +1,66 @@
+import numpy as np
+from star_pso.benchmarks.test_function import TestFunction
+
+
+class SixHumpCamelBack(TestFunction):
+    """
+    This function was originally proposed in:
+
+    Z. Michalewicz, Genetic Algorithms + Data Structures = Evolution Programs.
+    New York: Springer-Verlag, New York, 1996.
+    """
+
+    def __init__(self) -> None:
+        """
+        Default initializer of the SixHumpCamelBack class.
+        """
+
+        # Call the super initializer with the name.
+        super().__init__(name="Six_Hump_Camel_Back",
+                         x_min=np.array([-1.9, -1.1]),
+                         x_max=np.array([+1.9, +1.1]))
+    # _end_def_
+
+    def func(self, x_pos: np.ndarray) -> np.ndarray:
+        """
+        This is a 2D function with 2 global and 2 local optima.
+
+        :param x_pos: the current position(s) of the function.
+
+        :return: the function value(s).
+        """
+
+        # Initialize function values to NaN.
+        f_value = float("NaN")
+
+        # Separate the two variables.
+        x, y = x_pos
+
+        # Conditions for the different ranges.
+        x_range = (-1.9 <= x) & (x <= +1.9)
+        y_range = (-1.1 <= y) & (y <= +1.1)
+
+        # Vectorized calculations based on the condition.
+        if x_range & y_range:
+            f_value = -4 * ((4 - 2.1 * x**2 + (x**4)/3) * x**2 +
+                            x*y + 4*(y**2 - 1)*(y**2) )
+        # _end_if_
+
+        # Return the ndarray.
+        return f_value
+    # _end_def_
+
+    def initial_random_positions(self, n_pos: int = 50) -> np.ndarray:
+        """
+        Generate an initial set of uniformly random sampled positions
+        within the minimum / maximum bounds of the test problem.
+
+        :param n_pos: the number of positions to generate.
+
+        :return: a uniformly sampled set of random positions.
+        """
+        # Draw uniform random samples for the initial points.
+        return self.rng.uniform(self._x_min, self._x_max, size=(n_pos, 2))
+    # _end_def_
+
+# _end_class_
