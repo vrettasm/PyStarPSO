bfa5b1b5809748d0ecd1d9682ad9820e6db946de
michailvrettas <vrettasm@gmail.com>
2025-11-23 13:51:56 +0100

    Generalize Vincent to multiple dimensions.

--- star_pso/benchmarks/vincent.py
+++ star_pso/benchmarks/vincent.py
@@ -2,28 +2,38 @@ import numpy as np
 from star_pso.benchmarks.test_function import TestFunction
 
 
-class Vincent2D(TestFunction):
+class Vincent(TestFunction):
     """
     This function was originally proposed in:
 
-    O. Shir and T. Ba ̈ck, “Niche radius adaptation in the cms-es niching algorithm”,
+    O. Shir and T. Back, “Niche radius adaptation in the cms-es niching algorithm”,
     in Parallel Problem-Solving from Nature - PPSN IX, 9th International Conference
     (LNCS 4193). Reykjavík, Iceland: Springer, 2006, pp. 142 – 151.
     """
 
-    def __init__(self) -> None:
+    def __init__(self, n_dim: int = 2) -> None:
         """
-        Default initializer of the Vincent2D class.
+        Default initializer of the Vincent D class.
         """
 
+        # Ensure correct type.
+        n_dim = int(n_dim)
+
         # Call the super initializer with the name.
-        super().__init__(name="Vincent_2D",
+        super().__init__(name=f"Vincent_{n_dim}D",
                          x_min=0.25, x_max=10.0)
+        # Sanity check.
+        if n_dim < 2:
+            raise ValueError("Vincent D needs to be at least 2 dimensions.")
+        # _end_if_
+
+        # Assign the number of dimensions.
+        self.n_dim = n_dim
     # _end_def_
 
     def func(self, x_pos: np.ndarray) -> np.ndarray:
         """
-        This is a 2D function with 36 global optimal values.
+        This is a nD function with 6^n_dim global optimal values.
 
         :param x_pos: the current position(s) of the function.
 
@@ -31,24 +41,15 @@ class Vincent2D(TestFunction):
         """
 
         # Initialize function values to NaN.
-        f_value = float("NaN")
+        f_value = np.nan
 
-        # Separate the two variables.
-        x, y = x_pos
-
-        # Conditions for the different ranges.
-        x_range = (0.25 <= x) & (x <= 10.0)
-        y_range = (0.25 <= y) & (y <= 10.0)
-
-        # Check the range before computing.
-        if x_range & y_range:
+        # Check the valid function range.
+        if np.all((self.x_min <= x_pos) & (x_pos <= self.x_max)):
             # Compute the function value.
-            f_value = - (np.sin(10.0 * np.log(x)) +
-                         np.sin(10.0 * np.log(y))) / 2.0
-        # _end_if_
+            f_value = np.sum(np.sin(10.0 * np.log(x_pos)), axis=0)
 
         # Return the ndarray.
-        return f_value
+        return f_value/self.n_dim
     # _end_def_
 
     def initial_random_positions(self, n_pos: int = 50) -> np.ndarray:
@@ -61,7 +62,7 @@ class Vincent2D(TestFunction):
         :return: a uniformly sampled set of random positions.
         """
         # Draw uniform random samples for the initial points.
-        return self.rng.uniform(self._x_min, self._x_max, size=(n_pos, 2))
+        return self.rng.uniform(self._x_min, self._x_max, size=(n_pos, self.n_dim))
     # _end_def_
 
 # _end_class_
