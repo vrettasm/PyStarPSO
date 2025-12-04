eb1e76296d1018d8163fdc9db7683251fb576468
vrettasm <vrettasm@gmail.com>
2025-12-04 14:06:15 +0100

    Removed whitespace.

--- star_pso/engines/jack_of_all_trades_pso.py
+++ star_pso/engines/jack_of_all_trades_pso.py
@@ -262,7 +262,6 @@ class JackOfAllTradesPSO(GenericPSO):
 
         :return: None.
         """
-
         # Evaluates all the particles.
         for particle, velocity in zip(self.swarm.population,
                                       self._velocities):
