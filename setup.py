6150584cbfea7fd1552b10d1905eeb7d5bc4184c
michailvrettas <vrettasm@gmail.com>
2025-12-08 12:12:07 +0100

    Added setup.py and pyproject.toml files to the project.

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open("README.md", encoding="utf8") as readme_file:
+    readme_txt = readme_file.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='StarPSO',
+    version='0.1.0',
+    author='Michalis Vrettas, PhD',
+    author_email='michail.vrettas@gmail.com',
+    description='Particle Swarm Optimization algorithms toolbox in Python3',
+    long_description=readme_txt,
+    long_description_content_type='text/markdown',
+    url='https://github.com/vrettasm/PyStarPSO',
+    packages=find_packages(exclude=["docs", "unittests"]),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3',
+        'License :: GPL-3.0 License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+    install_requires=[
+        'numpy',
+        'scipy',
+        'numba',
+        'joblib',
+    ],
+)
