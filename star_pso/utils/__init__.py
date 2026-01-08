e627bef5f9591b35845d21d334e120ec2c9477a2
vrettasm <vrettasm@gmail.com>
2026-01-08 11:46:33 +0100

    Update documentation.

--- star_pso/utils/__init__.py
+++ star_pso/utils/__init__.py
@@ -9,12 +9,15 @@ Make a type alias for the position's type.
 
 VOptions = namedtuple("VOptions",
                       ["w0", "c1", "c2", "mode"], defaults=[False])
-"""
-Declare a named tuple with the parameters
-we want to use in the velocity equations:
 
-    - "w0": inertia weight
-    - "c1": cognitive coefficient
-    - "c2": social coefficient
-    - "mode": "fipso", "g_best", "multimodal".
-"""
+# Add documentation to VOptions.
+VOptions.__doc__ = """
+                   Declare a named tuple with the parameters
+                   we want to use in the velocity equations:
+
+                    - "w0": inertia weight
+                    - "c1": cognitive coefficient
+                    - "c2": social coefficient
+                    - "mode": "fipso", "g_best", "multimodal".
+                   """
+# _end_file_
