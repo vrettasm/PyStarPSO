9badd13de2bca1bbf9934c92e8aedbb2e322171e
vrettasm <vrettasm@gmail.com>
2026-01-08 11:40:21 +0100

    Update documentation.

--- star_pso/utils/__init__.py
+++ star_pso/utils/__init__.py
@@ -2,14 +2,19 @@ from typing import Union
 from collections import namedtuple
 from numpy.typing import ArrayLike
 
-# Make a type alias for the position's type.
 ScalarOrArray = Union[int, float, ArrayLike]
+"""
+Make a type alias for the position's type.
+"""
 
-# Declare a named tuple with the parameters
-# we want to use in the velocity equations:
-# 1) "w0": inertia weight
-# 2) "c1": cognitive coefficient
-# 3) "c2": social coefficient
-# 4) "mode": "fipso", "g_best", "multimodal".
 VOptions = namedtuple("VOptions",
                       ["w0", "c1", "c2", "mode"], defaults=[False])
+"""
+Declare a named tuple with the parameters
+we want to use in the velocity equations:
+
+    - "w0": inertia weight
+    - "c1": cognitive coefficient
+    - "c2": social coefficient
+    - "mode": "fipso", "g_best", "multimodal".
+"""
