c0c9aaf7c5542e3b1b298e2bf8dea19af6c73d05
vrettasm <vrettasm@gmail.com>
2026-01-12 14:27:17 +0100

    Update installation files.

--- setup.py
+++ setup.py
@@ -3,9 +3,6 @@ from setuptools import setup, find_packages
 with open("README.md", encoding="utf8") as readme_file:
     readme_txt = readme_file.read()
 
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
-
 setup(
     name='starpso',
     version='0.1.0',
@@ -21,7 +18,6 @@ setup(
         'Intended Audience :: Education',
         'Intended Audience :: Science/Research',
         'Programming Language :: Python :: 3',
-        'License :: GPL-3.0 License',
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.10',
