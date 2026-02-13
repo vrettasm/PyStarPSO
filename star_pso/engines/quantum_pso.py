0d1b4a3f7537ffc0230c0c1184d0db02121356d2
vrettasm <vrettasm@gmail.com>
2026-02-13 12:42:22 +0100

    Update citation format.

--- star_pso/engines/quantum_pso.py
+++ star_pso/engines/quantum_pso.py
@@ -17,8 +17,8 @@ class QuantumPSO(GenericPSO):
     This class implements a variant of the quantum particle swarm optimization
     as described in:
 
-    - M. Xi, J. Sun, W. Xu, An improved quantum-behaved particle swarm optimization
-      algorithm with weighted mean best position, Applied Mathematics and Computation
+    - M. Xi, J. Sun, W. Xu, "An improved quantum-behaved particle swarm optimization
+      algorithm with weighted mean best position", Applied Mathematics and Computation
       205 (2008) 751–759,
     """
 
