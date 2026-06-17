83957fa98252abded3cd6ab60661b73d92ac7d63
michail vrettas <vrettasm@gmail.com>
2026-06-17 15:44:42 +0200

    Enable cache and nogil options.

--- star_pso/engines/binary_pso.py
+++ star_pso/engines/binary_pso.py
@@ -7,7 +7,7 @@ from star_pso.engines.generic_pso import GenericPSO
 from star_pso.utils.auxiliary import (nb_clip_inplace,
                                       nb_median_hamming_distance)
 
-@njit(fastmath=True)
+@njit(cache=True, nogil=True, fastmath=True)
 def fast_logistic(x: NDArray) -> NDArray:
     """
     Local auxiliary function that is used to compute
