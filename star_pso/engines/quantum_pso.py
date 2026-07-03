0378b3e4a834fc53087ce33373fc2d0ffead9e5d
michail vrettas <vrettasm@gmail.com>
2026-07-03 09:57:40 +0200

    Added type hints and update documentation.

--- star_pso/engines/quantum_pso.py
+++ star_pso/engines/quantum_pso.py
@@ -1,6 +1,8 @@
+# Third party imports.
 from numpy import log, where
-from numpy.typing import ArrayLike
+from numpy.typing import (NDArray, ArrayLike)
 
+# Custom code imports.
 from star_pso.utils import VOptions
 from star_pso.engines.generic_pso import GenericPSO
 from star_pso.utils.auxiliary import (nb_clip_inplace,
@@ -17,9 +19,9 @@ class QuantumPSO(GenericPSO):
     This class implements a variant of the quantum particle swarm optimization
     as described in:
 
-    - M. Xi, J. Sun, W. Xu (2008), "An improved quantum-behaved particle swarm optimization
-      algorithm with weighted mean best position", Applied Mathematics and Computation vol.
-      205 pp: 751–759, doi: 10.1016/j.amc.2008.05.135.
+    - M. Xi, J. Sun, W. Xu (2008), "An improved quantum-behaved particle swarm
+      optimization algorithm with weighted mean best position", Applied Mathematics
+      and Computation vol. 205 pp: 751–759, doi: 10.1016/j.amc.2008.05.135.
     """
 
     def __init__(self, x_min: ArrayLike, x_max: ArrayLike, **kwargs) -> None:
@@ -60,22 +62,22 @@ class QuantumPSO(GenericPSO):
         arr_shape = (self.n_rows, self.n_cols)
 
         # Pre-sample the 'phi' parameters.
-        param_phi = GenericPSO.rng.random(size=arr_shape)
+        param_phi: NDArray = GenericPSO.rng.random(size=arr_shape)
 
         # Pre-sample the 'u' parameters.
-        param_u = GenericPSO.rng.random(size=arr_shape)
+        param_u: NDArray = GenericPSO.rng.random(size=arr_shape)
 
         # Get the (Global / Local / FIPSO) best positions.
-        m_best = self.get_local_best_positions(params.mode.lower())
+        m_best: NDArray = self.get_local_best_positions(params.mode.lower())
 
         # Extract the current positions.
-        x_current = self.swarm.positions_as_array()
+        x_current: NDArray = self.swarm.positions_as_array()
 
         # Extract the best (historical) positions.
-        p_best = self.swarm.best_positions_as_array()
+        p_best: NDArray = self.swarm.best_positions_as_array()
 
         # Extract the global best position.
-        g_best = self.swarm.best_particle().position
+        g_best: NDArray = self.swarm.best_particle().position
 
         # Construct the 'p_best' (in-place).
         p_best *= param_phi
@@ -89,10 +91,10 @@ class QuantumPSO(GenericPSO):
         param_u[param_u <= 1.0e-256] = QuantumPSO.NUMPY_EPS
 
         # Compute the offset.
-        p_offset = beta_coefficient * (m_best - x_current) * log(param_u)
+        p_offset: NDArray = beta_coefficient * (m_best - x_current) * log(param_u)
 
         # Select the directions at random.
-        direction = where(self.rng.random(arr_shape) < 0.5, -1.0, 1.0)
+        direction: NDArray = where(self.rng.random(arr_shape) < 0.5, -1.0, 1.0)
 
         # Perform all operations in place.
         p_best += direction * p_offset
