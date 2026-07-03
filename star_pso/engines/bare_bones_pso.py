a7d3db149ebcbacaec23b334f0c5ca17a2f1f8dd
michail vrettas <vrettasm@gmail.com>
2026-07-03 15:31:26 +0200

    Added type hint.

--- star_pso/engines/bare_bones_pso.py
+++ star_pso/engines/bare_bones_pso.py
@@ -38,10 +38,10 @@ class BareBonesPSO(GenericPSO):
         self.disable_parameters_update()
 
         # Generate initial particle "velocities".
-        self._velocities = GenericPSO.rng.uniform(self.lower_bound,
-                                                  self.upper_bound,
-                                                  size=(self.n_rows,
-                                                        self.n_cols))
+        self._velocities: NDArray = GenericPSO.rng.uniform(self.lower_bound,
+                                                           self.upper_bound,
+                                                           size=(self.n_rows,
+                                                                 self.n_cols))
     # _end_def_
 
     def update_velocities(self, params: VOptions) -> None:
