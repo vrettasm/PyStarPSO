b102b6692120c43dffcf4f2f38d8829e89457547
michail vrettas <vrettasm@gmail.com>
2026-07-07 11:14:02 +0200

    Added type hints.

--- star_pso/engines/quantum_pso.py
+++ star_pso/engines/quantum_pso.py
@@ -126,10 +126,10 @@ class QuantumPSO(GenericPSO):
         :return: None.
         """
         # Generate uniform FLOAT positions U(x_min, x_max).
-        self._velocities = GenericPSO.rng.uniform(self.lower_bound,
-                                                  self.upper_bound,
-                                                  size=(self.n_rows,
-                                                        self.n_cols))
+        self._velocities: NDArray = GenericPSO.rng.uniform(self.lower_bound,
+                                                           self.upper_bound,
+                                                           size=(self.n_rows,
+                                                                 self.n_cols))
         # Assign the new positions.
         self.swarm.set_positions(self._velocities)
     # _end_def_
