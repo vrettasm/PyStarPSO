fc939d72ac773292c9b012be7de2fbe812bc5d78
michail vrettas <vrettasm@gmail.com>
2026-07-07 12:57:14 +0200

    fix typo.

--- star_pso/engines/standard_pso.py
+++ star_pso/engines/standard_pso.py
@@ -75,7 +75,7 @@ class StandardPSO(GenericPSO):
         """
         # Reset particle velocities.
         self._velocities: NDArray = GenericPSO.rng.uniform(
-            low=-1.0, high=+1.0, size=(self.n_rows, self.n_cols)
+            low=-1.0, high=1.0, size=(self.n_rows, self.n_cols)
         )
         # Generate random uniform positions.
         self.generate_random_positions()
