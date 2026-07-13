19824657c47fe0dc36774d04673f16d6156e68ff
michail vrettas <vrettasm@gmail.com>
2026-07-13 12:55:11 +0200

    Added Alpine function.

--- star_pso/benchmarks/composite_functions.py
+++ star_pso/benchmarks/composite_functions.py
@@ -93,21 +93,41 @@ def f_ackley(x_pos: NDArray, alpha: float = 20.0,
     return f_total + alpha + np.e
 # _end_def_
 
+# Basic function: 6
+@njit(cache=True, fastmath=True)
+def f_alpine(x_pos: NDArray) -> NDArray:
+    """
+    Computes the Alpine function at x_pos.
+    """
+    # Compute the equation.
+    f_total: NDArray = np.sum(np.abs(x_pos * np.sin(x_pos)) + 0.1 * x_pos)
+    # Return final result.
+    return f_total
+# _end_def_
+
 # Auxiliary dictionary with the basis functions.
 BASIS_FUNCTIONS: dict = {"f_ackley": f_ackley,
+                         "f_alpine": f_alpine,
                          "f_sphere": f_sphere,
                          "f_griewank": f_griewank,
                          "f_rastrigin": f_rastrigin,
                          "f_weierstrass": f_weierstrass}
+
 """
 Define a dictionary with all the basis functions:
 
     - Ackley
+    - Alpine
     - Sphere
     - Griewank
     - Rastrigin
     - Weierstrass
+
+NOTE: These functions are not vectorized!
 """
+
+# Public interface.
+__all__ = ["CompositeFunction", "BASIS_FUNCTIONS"]
 
 
 class CompositeFunction(TestFunction):
