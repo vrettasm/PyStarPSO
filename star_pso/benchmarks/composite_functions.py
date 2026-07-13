2e1e27e17eb0d3996857e69a1ced6f876f43fee1
michail vrettas <vrettasm@gmail.com>
2026-07-13 15:23:39 +0200

    Update return value.

--- star_pso/benchmarks/composite_functions.py
+++ star_pso/benchmarks/composite_functions.py
@@ -346,7 +346,7 @@ class CompositeFunction(TestFunction):
         num_optima: int = len(found_optima)
 
         # Return the tuple (number of found, total number)
-        return num_optima, len(self.basis_f)
+        return num_optima, 1
     # _end_def_
 
     def __str__(self) -> str:
